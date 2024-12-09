f82db2329819994c92da03ac782519c6f5d0eef3
NatalyaBoreisha <08bornata@gmail.com>
2024-12-09 10:36:49 +0300

    Revert "Merge pull request #1 from NatalyaBoreisha/hometask-4"
    This reverts commit d473d5c20e62b9e0da1dd5ef515c7736c5cfd2dc, reversing
    changes made to f84f182c9e08fd144c654d09fad8b34c54daeddf.

--- script.js
+++ script.js
@@ -7,30 +7,3 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
-
-
-const input = document.getElementById("input");
-const items = document.getElementById("items");
-
-input.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        const inputValue = input.value;
-
-        if (inputValue) {
-            addItem(inputValue);
-            input.value = "";
-        }
-    }
-});
-
-function addItem(text) {
-    const item = document.createElement("div");
-    item.className = "item";
-    item.textContent = text;
-
-    item.addEventListener("click", () => {
-        item.classList.toggle("done");
-    });
-
-    items.appendChild(item);
-}
